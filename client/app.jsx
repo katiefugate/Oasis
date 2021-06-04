40c084a3557c5796d9e88b3a63b51157f75da28f
katiefugate <katiiierochelle@yahoo.com>
2021-06-04 22:29:12 +0000

    completed user can sign in as a host or swimmer.

--- client/app.jsx
+++ client/app.jsx
@@ -11,16 +11,21 @@ import PoolInfo from './pages/pool-info';
 import HostNavbar from './components/host-navbar';
 import HostBookingRequests from './pages/host-booking-requests';
 import SignUp from './pages/sign-up';
+import SignIn from './pages/sign-in';
+import decodeToken from './lib/decode-token';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
     this.state = {
       route: parseRoute(window.location.hash),
       type: null,
       swimmerId: null,
-      hostId: null
+      hostId: null,
+      isAuthorizing: true,
+      invalid: 'hidden'
     };
   }
 
@@ -30,6 +35,26 @@ export default class App extends React.Component {
         route: parseRoute(window.location.hash)
       });
     });
+    const token = window.localStorage.getItem('oasis-user');
+    const user = token ? decodeToken(token) : null;
+    if (!user) {
+      window.location.hash = '#';
+      this.setState({ isAuthorizing: false });
+    } else if (user.type === 'swimmer') {
+      this.setState({
+        type: 'swimmer',
+        swimmerId: user.swimmerId,
+        hostId: null,
+        isAuthorizing: false
+      });
+    } else {
+      this.setState({
+        type: 'host',
+        swimmerId: null,
+        hostId: user.hostId,
+        isAuthorizing: false
+      });
+    }
   }
 
   handleSignUp(type, id) {
@@ -48,6 +73,30 @@ export default class App extends React.Component {
     }
   }
 
+  handleSignIn(type, result) {
+    const { user, token } = result;
+    if (result.error) {
+      this.setState({ invalid: 'invalid' });
+      return;
+    }
+    window.localStorage.setItem('oasis-user', token);
+    if (type === 'swimmer') {
+      this.setState({
+        type: 'swimmer',
+        swimmerId: user.swimmerId,
+        hostId: null,
+        invalid: 'valid'
+      });
+    } else {
+      this.setState({
+        type: 'host',
+        swimmerId: null,
+        hostId: user.hostId,
+        invalid: 'valid'
+      });
+    }
+  }
+
   renderPage() {
     const { path } = this.state.route;
     const { params } = this.state.route;
@@ -58,6 +107,9 @@ export default class App extends React.Component {
     }
     if (path === 'sign-up') {
       return < SignUp onSignUp={this.handleSignUp} />;
+    }
+    if (path === 'sign-in') {
+      return < SignIn onSignIn={this.handleSignIn} invalid={this.state.invalid}/>;
     }
     if (path === 'host-form') {
       return (
@@ -107,6 +159,7 @@ export default class App extends React.Component {
   }
 
   render() {
+    if (this.state.isAuthorizing) return null;
     return (
       <>
       {this.renderPage()}
--- client/pages/sign-in.jsx
+++ client/pages/sign-in.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class SignUp extends React.Component {
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -8,48 +8,56 @@ class SignUp extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const name = event.target.name.value;
     const username = event.target.username.value;
     const password = event.target.password.value;
     if (event.target.user.value === 'host') {
       const init = {
         method: 'POST',
-        body: JSON.stringify({ name, username, password }),
+        body: JSON.stringify({ username, password }),
         headers: {
           'Content-Type': 'application/json'
         }
       };
-      fetch('/api/host', init)
+      fetch('/api/host/sign-in', init)
         .then(response => response.json())
         .then(body => {
-          this.props.onSignUp('host', body.hostId);
+          this.props.onSignIn('host', body);
         });
-      window.location.hash = '#sign-in';
+      if (this.props.invalid === 'invalid') {
+        window.location.hash = 'sign-in';
+      } else {
+        window.location.hash = '#host-form';
+      }
     } else {
       const init = {
         method: 'POST',
-        body: JSON.stringify({ name, username, password }),
+        body: JSON.stringify({ username, password }),
         headers: {
           'Content-Type': 'application/json'
         }
       };
-      fetch('/api/swimmer', init)
+      fetch('/api/swimmer/sign-in', init)
         .then(response => response.json())
         .then(body => {
-          this.props.onSignUp('swimmer', body.swimmerId);
+          this.props.onSignIn('swimmer', body);
         });
-      window.location.hash = '#sign-in';
+      if (this.props.invalid) {
+        window.location.hash = 'sign-in';
+      } else {
+        window.location.hash = '#search';
+      }
     }
   }
 
   render() {
     return (
       <div>
-        <div className='header-sign-up'>
+        <div className='header-sign-in'>
           <h1 className='header-name'>OASIS</h1>
         </div>
-        <form onSubmit={this.handleSubmit} className='sign-up-form'>
-          <h1 className='auth-header'>Create an account to get started!</h1>
+        <div className={this.props.invalid}>Invalid Login!</div>
+        <form onSubmit={this.handleSubmit} className='sign-in-form'>
+          <h1 className='auth-header'>Sign in to continue</h1>
           <div className='radio-container'>
             <input type='radio' name='user' id='swimmer' value='swimmer'></input>
             <label htmlFor='swimmer'>Swimmer</label>
@@ -57,22 +65,18 @@ class SignUp extends React.Component {
             <label htmlFor="host">Host</label>
           </div>
           <div className='input-container'>
-            <label className='sign-up-label' htmlFor='name'>Name</label>
-            <input className='sign-up-input' type='name' name='name'></input>
+            <label className='sign-in-label' htmlFor='username'>Username</label>
+            <input className='sign-in-input' type='username' name='username'></input>
           </div>
           <div className='input-container'>
-            <label className='sign-up-label' htmlFor='username'>Username</label>
-            <input className='sign-up-input' type='username' name='username'></input>
+            <label className='sign-in-label' htmlFor="password">Password</label>
+            <input className='sign-in-input' type="password" name="password"></input>
           </div>
-          <div className='input-container'>
-            <label className='sign-up-label' htmlFor="password">Password</label>
-            <input className='sign-up-input' type="password" name="password"></input>
-          </div>
-          <button className='sign-up-button' type='submit'>Sign Up</button>
+          <button className='sign-in-button' type='submit'>Sign in!</button>
         </form>
       </div>
     );
   }
 }
 
-export default SignUp;
+export default SignIn;
