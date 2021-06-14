9e3d7dc98ad22522632c08ffbd2d124fd82dfb56
katiefugate <katiiierochelle@yahoo.com>
2021-06-14 22:53:33 +0000

    completed polish-2

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -7,6 +7,9 @@ class Home extends React.Component {
         <div className='header-home'>
           <h1 className='header-title'>OASIS</h1>
         </div>
+        <div className='home-description'>Get away to your own private oasis. Rent a pool by the hour.</div>
+        <p className='home-or'>OR</p>
+        <div className='home-host'>Become a host and turn your splash into cash!</div>
         <div className='user-buttons-container'>
           <a className='sign-up-link' href='#sign-up'>
             <button className='home-button'>Sign Up</button>
