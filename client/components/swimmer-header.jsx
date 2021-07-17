c3a32a687f66830718385cbbb016e6e9bcc22454
katiefugate <katiiierochelle@yahoo.com>
2021-07-17 02:58:29 +0000

    added bookings icon to swimmer navbar.

--- client/components/swimmer-header.jsx
+++ client/components/swimmer-header.jsx
@@ -12,11 +12,14 @@ class SwimmerHeader extends React.Component {
 
   render() {
     return (
-  <div className='header'>
-    <button onClick={this.handleClick} className='sign-out-button'>Sign out</button>
-    <h1 className='header-title'>OASIS</h1>
-        <div className='desktop-links'><a className='search-link' href='#search'>Search</a></div>
-  </div>
+    <div className='header'>
+      <button onClick={this.handleClick} className='sign-out-button'>Sign out</button>
+      <h1 className='header-title'>OASIS</h1>
+      <span className='desktop-links'>
+        <a className='search-link' href='#search'>Search</a>
+        <a className='booking-requests-link swimmer' href='#swimmer-bookings'>Bookings</a>
+      </span>
+    </div>
     );
   }
 }
