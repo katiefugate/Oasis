757cbfcc66fdcb06e47d54d4a71e31dcdb8f7a2b
katiefugate <katiiierochelle@yahoo.com>
2021-08-25 06:12:42 +0000

    added red circle to booking requests icon

--- client/components/host-navbar.jsx
+++ client/components/host-navbar.jsx
@@ -5,7 +5,7 @@ class HostNavbar extends React.Component {
   render() {
     return (
       <div className='navbar'>
-        <a href='#host-bookings'><i className="fas fa-calendar-plus host-bookings-icon"></i></a>
+        <a className='booking-a' href='#host-bookings'><i className="fas fa-circle notification"></i><i className="fas fa-calendar-plus host-bookings-icon"></i></a>
         <a href='#host-pools'><i className="fas fa-swimming-pool host-pools-icon"></i></a>
         <a href='#host-upcoming'><i className="fas fa-calendar-alt host-booked-icon"></i></a>
       </div>
