69808aa4f92807fb87d3aa805c031935a81a7d06
Katie <katiiierochelle@yahoo.com>
2021-09-17 01:49:06 +0000

    added toggle to header.

--- client/components/host-header.jsx
+++ client/components/host-header.jsx
@@ -4,10 +4,22 @@ class HostHeader extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.toggleHandler = this.toggleHandler.bind(this);
+    this.state = {
+      type: 'swimmer'
+    };
   }
 
   handleClick() {
     this.props.onSignOut();
+  }
+
+  toggleHandler() {
+    if (this.state.type === 'host') {
+      this.setState({ type: 'swimmer' });
+    } else {
+      this.setState({ type: 'host' });
+    }
   }
 
   render() {
@@ -15,6 +27,10 @@ class HostHeader extends React.Component {
       <div className='header'>
         <button onClick={this.handleClick} className='sign-out-button'>Sign out</button>
         <h1 className='header-title'>OASIS</h1>
+        <p className={`toggle-${this.state.type}`} onClick={this.toggleHandler}>
+          {this.state.type}
+          <span className={`circle ${this.state.type}`} onClick={this.toggleHandler}></span>
+        </p>
         <span className='desktop-links'>
           <a className='booking-requests-link' href='#host-bookings'>Booking Requests</a>
           <a className='my-pools-link' href='#host-pools'>My Pools</a>
